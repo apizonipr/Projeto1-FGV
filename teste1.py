9a741f7eb0f0493a4c3a140e22f3936426611294
alicia <aliciaprevot6@gmail.com>
2026-04-28 15:21:43 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("A falta da pesença de Boybands decentes em 2026")
+st.title("A falta da presença de Boybands decentes em 2026")
 st.write("Precisamos debater em como a não presença de boybands em 2026 prejudica a percepção da infância")
 
 st.image("https://assets.portalleodias.com/2024/04/1968846635-one-direction.webp")
