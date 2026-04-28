c02a852a6fae26a6662c90d6eb0b09ca22f407d2
alicia <aliciaprevot6@gmail.com>
2026-04-28 15:12:19 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,3 +1,3 @@
 # Projeto1-FGV
 
-Oi este é o meu projeto em python. É um teste em sala de aula com os meus queridos discentes!
+Esse é o meu projeto em python. É um teste em sala de aula com os meus queridos discentes!
